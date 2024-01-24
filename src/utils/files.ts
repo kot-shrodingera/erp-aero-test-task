0f62a0d9ca79e51e7a3872034f89970b8b86b7dc
Kot Shrodingera <kot.shrodingera@gmail.com>
2024-01-24 09:03:29 +0100

    splitFilename: fix for extensionless files

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,5 +1,11 @@
 export const splitFilename = (filename: string) => {
   const index = filename.lastIndexOf('.')
+  if (index === -1) {
+    return {
+      name: filename,
+      extension: '',
+    }
+  }
   return {
     name: filename.substring(0, index),
     extension: filename.substring(index),
